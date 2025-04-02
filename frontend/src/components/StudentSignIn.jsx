b5cbb89d5ba5b5b466f35d4ede8773eb6c2411b4
ishaaaa64 <kaliramanisha64@gmail.com>
2025-04-02 22:07:23 +0300

    cssisha

--- frontend/src/components/StudentSignIn.jsx
+++ frontend/src/components/StudentSignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StudentSignInContainer, FormContainer, InputField, SubmitButton } from '../styles/StudentSignInStyles';
+import { StudentSignInContainer, FormContainer, InputField, SubmitButton, Heading } from '../styles/StudentSignInStyles';
 import { useNavigate } from 'react-router-dom';
 
 const StudentSignIn = () => {
@@ -10,40 +10,36 @@ const StudentSignIn = () => {
   const handleSignIn = async (e) => {
     e.preventDefault();
 
-    // Check if email and password are provided
     if (!email || !password) {
       alert('Please enter both email and password');
       return;
     }
 
-      try {
-        // Send POST request to backend service
-        const response = await fetch('http://localhost:4000/api/v1/students/s-login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),  // Include password here if needed
-        });
+    try {
+      const response = await fetch('http://localhost:4000/api/v1/students/s-login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-        const data = await response.json();
-        if (response.ok) {
-          localStorage.setItem("student", JSON.stringify(data.student));
-          // Successful email sent, redirect to dashboard
-          alert(data.message || 'Logged in successfully!');
-          navigate('/student/dashboard');
-        } else {
-          alert(data.error || 'Failed to log in');
-        }
-      } catch (error) {
-        // Handle any error during the request
-        alert('Error occurred: ' + error.message);
+      const data = await response.json();
+      if (response.ok) {
+        localStorage.setItem("student", JSON.stringify(data.student));
+        alert(data.message || 'Logged in successfully!');
+        navigate('/student/dashboard');
+      } else {
+        alert(data.error || 'Failed to log in');
       }
+    } catch (error) {
+      alert('Error occurred: ' + error.message);
+    }
   };
 
   return (
     <StudentSignInContainer>
-      <h2>Student Sign In</h2>
+      <Heading>Student Sign In</Heading>
       <FormContainer onSubmit={handleSignIn}>
         <InputField
           type="email"
